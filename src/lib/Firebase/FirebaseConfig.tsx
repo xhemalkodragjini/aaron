3089476f61cc372af92b03472da629d8392b9bb5
jakobap <jakobpoerschmann@gmail.com>
2025-02-10 14:08:11 +0100

    firebase config application default creds auth

--- src/lib/Firebase/FirebaseConfig.tsx
+++ src/lib/Firebase/FirebaseConfig.tsx
@@ -1,21 +1,12 @@
 import { initializeApp, getApps } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-import { getAuth } from 'firebase/auth'
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB8eV17zj0jS6yetnE1fKEVG3Ztt_h5xIA",
-  authDomain: "poerschmann-ce-intern.firebaseapp.com",
-  projectId: "poerschmann-ce-intern",
-  storageBucket: "poerschmann-ce-intern.firebasestorage.app",
-  messagingSenderId: "1008828657292",
-  appId: "1:1008828657292:web:18d185635fc272a2e815a9",
-  measurementId: "G-VRB7R9GV3W"
+  projectId:process.env.GCP_PROJECT_ID,
 };
 
 const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApps()[0];
 
-// const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
-const auth = getAuth(app);
 
-export { app, db, auth };
+export { app, db };
